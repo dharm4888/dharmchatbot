6478c7ff53ed0fabba771eb04c59b416b02abe71
dharm4888 <dharm.oss4888@gmail.com>
2026-02-21 19:04:13 +0530

    Update import for Document from langchain_core

--- backend/utils.py
+++ backend/utils.py
@@ -7,7 +7,7 @@ import pdfplumber
 from openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.docstore.document import Document
+from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # ==========================
